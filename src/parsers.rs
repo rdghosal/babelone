65ba2367068c2fc81d41a6d4daf66e502891ae39
Rahul Ghosal <rdghosal@gmail.com>
2024-03-03 09:02:15 -0500

    refac: use rustpython parser to parse setup using ast

--- src/parsers.rs
+++ src/parsers.rs
@@ -1,11 +1,11 @@
-use pyo3::exceptions::{PySyntaxError, PyValueError};
-use regex::Regex;
+use pyo3::exceptions::PyValueError;
+use rustpython_parser::{ast, Parse};
 use serde::Deserialize;
 use std::{collections::BTreeMap, error::Error, fs, path::Path};
 
-type Dependency = (String, Option<String>);
+type Dependency = String;
 
-trait BuildSpecFile {
+trait BuildSpecParser {
     fn from_file(path: &Path) -> Result<Self, Box<dyn Error>>
     where
         Self: Sized;
@@ -15,7 +15,7 @@ struct Requirements {
     requires: Vec<Dependency>,
 }
 
-impl BuildSpecFile for Requirements {
+impl BuildSpecParser for Requirements {
     fn from_file(path: &Path) -> Result<Self, Box<dyn Error>> {
         let mut requires = Vec::<Dependency>::new();
         let lines = read_file(&path)?;
@@ -24,10 +24,7 @@ impl BuildSpecFile for Requirements {
             if line.is_empty() {
                 continue;
             }
-            let mut split = line.split("==").map(|s| s.trim());
-            let name = split.next().unwrap();
-            let version = split.next();
-            requires.push((name.to_string(), version.map(|v| v.trim().to_string())));
+            requires.push(line.trim().replace(" ", "").to_string());
         }
         Ok(Self { requires })
     }
@@ -43,87 +40,195 @@ struct SetupPy {
 }
 
 impl SetupPy {
-    fn get_dep_from_setup(
-        setup_kwargs: &BTreeMap<String, String>,
-        kw: &str,
-    ) -> Option<Vec<Dependency>> {
-        if let Some(args) = setup_kwargs.get(kw) {
-            let mut deps = Vec::<Dependency>::new();
-            for arg in args.split(',') {
-                if arg.is_empty() {
-                    continue;
-                }
-                let mut split = arg.split("==").map(|s| s.trim());
-                let (package, version) = (
-                    split.next().unwrap().to_string(),
-                    split.next().map(|s| s.to_string()),
-                );
-                deps.push((package, version))
+    fn parse_string(
+        expr: &ast::Expr,
+        assignments: &BTreeMap<String, ast::Expr>,
+    ) -> Result<String, pyo3::PyErr> {
+        match expr {
+            ast::Expr::Constant(_) => {
+                return Ok(expr.to_string()?);
             }
-            Some(deps)
-        } else {
-            None
+            ast::Expr::Name(name) => {
+                if let Some(v) = assignments.get(&name.id.to_string()) {
+                    return Ok(v.to_string()?);
+                }
+            }
+            _ => (),
         }
+        return Err(PyValueError::new_err("Failed to parse string."));
+    }
+    fn parse_string_vec(
+        expr: &ast::Expr,
+        assignments: &BTreeMap<String, ast::Expr>,
+    ) -> Result<Vec<String>, pyo3::PyErr> {
+        match expr {
+            ast::Expr::List(_) => {
+                return Ok(expr.to_string_vec()?);
+            }
+            ast::Expr::Name(name) => {
+                if let Some(v) = assignments.get(&name.id.to_string()) {
+                    return Ok(v.to_string_vec()?);
+                }
+            }
+            _ => (),
+        }
+        return Err(PyValueError::new_err("Failed to parse Vec<String>."));
     }
 }
 
-impl BuildSpecFile for SetupPy {
+trait PyStrList {
+    fn to_string_vec(&self) -> Result<Vec<String>, pyo3::PyErr>;
+}
+trait PyStr {
+    fn to_string(&self) -> Result<String, pyo3::PyErr>;
+}
+impl PyStrList for ast::Expr {
+    fn to_string_vec(&self) -> Result<Vec<String>, pyo3::PyErr> {
+        if let ast::Expr::List(list) = &self {
+            let mut result = Vec::<String>::new();
+            for element in &list.elts {
+                if let ast::Expr::Constant(c) = element {
+                    if let ast::Constant::Str(s) = &c.value {
+                        result.push(s.clone());
+                    }
+                }
+            }
+            return Ok(result);
+        }
+        return Err(PyValueError::new_err(
+            "Failed to parse Expr as Vec<String>.",
+        ));
+    }
+}
+impl PyStr for ast::Expr {
+    fn to_string(&self) -> Result<String, pyo3::PyErr> {
+        if let ast::Expr::Constant(c) = &self {
+            if let ast::Constant::Str(s) = &c.value {
+                return Ok(s.clone());
+            }
+        }
+        return Err(PyValueError::new_err(
+            "Failed to parse String value from ExprConstant.",
+        ));
+    }
+}
 
-    // TODO: handle mapping identifiers, and values therein, to setup kwargs.
-    // e.g., 
-    // ```python
-    // NAME = 'my-project'
-    //
-    // setup(name=NAME)
-    //```
+trait SetupPyNode {
+    fn get_setup_call(&self) -> Option<&ast::ExprCall>;
+}
+
+impl SetupPyNode for ast::StmtExpr {
+    fn get_setup_call(&self) -> Option<&ast::ExprCall> {
+        if let ast::Expr::Call(c) = self.value.as_ref() {
+            let is_setup = match c.func.as_ref() {
+                ast::Expr::Name(n) => "setup" == n.id.as_str(),
+                ast::Expr::Attribute(a) => "setup" == a.attr.as_str(),
+                _ => false,
+            };
+            if is_setup {
+                return Some(c);
+            }
+        };
+        return None;
+    }
+}
+
+trait IdentValueMap {
+    fn insert_assignments(&mut self, assignment: &ast::StmtAssign) -> &mut Self;
+}
+
+impl IdentValueMap for BTreeMap<String, ast::Expr> {
+    fn insert_assignments(&mut self, assignment: &ast::StmtAssign) -> &mut Self {
+        let mut identifiers = Vec::<String>::new();
+        for target in assignment.targets.iter() {
+            let ast::Expr::Name(e) = target else {
+                panic!("Expected name of Expr::Name in assignment parsing.");
+            };
+            let identifier = e.id.to_string();
+            identifiers.push(identifier);
+        }
+        for identifier in identifiers {
+            self.insert(identifier, *assignment.value.clone());
+        }
+        self
+    }
+}
+
+impl SetupPy {
+    fn get_setup_node<'a>(
+        statements: &'a Vec<ast::Stmt>,
+        idx: &mut usize,
+        assignments: &'a mut BTreeMap<String, ast::Expr>,
+    ) -> Result<Option<(&'a ast::ExprCall, &'a mut BTreeMap<String, ast::Expr>)>, Box<dyn Error>>
+    {
+        if *idx < statements.len() {
+            match &statements[*idx] {
+                ast::Stmt::Expr(expr) => {
+                    let setup_call = expr.get_setup_call();
+                    if setup_call.is_some() {
+                        return Ok(Some((setup_call.unwrap(), assignments)));
+                    }
+                }
+                ast::Stmt::Assign(assignment) => {
+                    assignments.insert_assignments(assignment);
+                }
+                ast::Stmt::If(if_stmt) => {
+                    return Self::get_setup_node(&if_stmt.body, &mut 0, assignments);
+                }
+                _ => (),
+            }
+            *idx += 1;
+            return Self::get_setup_node(statements, idx, assignments);
+        }
+        return Ok(None);
+    }
+}
+
+impl BuildSpecParser for SetupPy {
     fn from_file(path: &Path) -> Result<Self, Box<dyn Error>>
     where
         Self: Sized,
     {
-        let contents = read_file(&path)?.replace('\n', "");
-        let re = Regex::new(r".*setup\((.*)\)").unwrap();
-        let Some(setup) = re.captures(&contents) else {
-            return Err(Box::new(PyValueError::new_err(
-                "Failed to parse setup.py. Invocation of a `setup` callable not found.",
-            )));
-        };
-        let mut kwargs = BTreeMap::<String, String>::new();
-        let (mut kw, mut arg) = (String::new(), String::new());
-        let (mut kw_done, mut is_list_arg) = (false, false);
-        for char in setup[1].replace('"', "").replace("'", "").chars() {
-            match (char, kw_done) {
-                ('[', true) => is_list_arg = true,
-                (']', true) => is_list_arg = false,
-                (',', true) if is_list_arg => {
-                    arg.push(char);
-                }
-                (',', true) if !is_list_arg => {
-                    kwargs.insert(kw.trim().to_string(), arg.trim().to_string());
-                    kw.clear();
-                    arg.clear();
-                    (kw_done, is_list_arg) = (false, false);
-                }
-                ('=', false) => kw_done = true,
-                (_, true) => {
-                    arg.push(char);
-                }
-                (_, false) => {
-                    kw.push(char);
+        let contents = read_file(&path)?;
+        let mut assignments = BTreeMap::<String, ast::Expr>::new();
+        let statements = ast::Suite::parse(&contents, &path.to_str().unwrap())?;
+
+        let mut package_name: Option<String> = None;
+        let mut version: Option<String> = None;
+        let mut install_requires: Option<Vec<Dependency>> = None;
+        let mut setup_requires: Option<Vec<Dependency>> = None;
+        let mut dev_requires: Option<Vec<Dependency>> = None;
+
+        if let Some((setup, assignments)) =
+            Self::get_setup_node(&statements, &mut 0, &mut assignments)?
+        {
+            for keyword in &setup.keywords {
+                let ident = keyword.arg.clone().unwrap();
+                match ident.as_str() {
+                    "name" => {
+                        package_name = Some(Self::parse_string(&keyword.value, &assignments)?)
+                    }
+                    "version" => version = Some(Self::parse_string(&keyword.value, &assignments)?),
+                    "install_requires" => {
+                        install_requires = Some(Self::parse_string_vec(&keyword.value, &assignments)?);
+                    }
+                    "dev_requires" => {
+                        dev_requires = Some(Self::parse_string_vec(&keyword.value, &assignments)?);
+                    }
+                    "setup_requires" => {
+                        setup_requires = Some(Self::parse_string_vec(&keyword.value, &assignments)?);
+                    }
+                    _ => continue,
                 }
             }
         }
-        let Some(package_name) = kwargs.get("name") else {
+        if package_name.is_none() {
             return Err(Box::new(PyValueError::new_err(
-                "Failed to parse required `package_name` from setup.",
+                "package_name must be defined",
             )));
-        };
-        let package_name = package_name.trim().to_string();
-        let version = kwargs.get("version").map(|v| v.to_string());
-        let install_requires = Self::get_dep_from_setup(&kwargs, "install_requires");
-        let dev_requires = Self::get_dep_from_setup(&kwargs, "dev_requires");
-        let setup_requires = Self::get_dep_from_setup(&kwargs, "setup_requires");
+        }
         Ok(Self {
-            package_name,
+            package_name: package_name.unwrap(),
             version,
             install_requires,
             dev_requires,
@@ -153,7 +258,7 @@ struct Project {
     dependencies: Option<Vec<String>>,
 }
 
-impl BuildSpecFile for PyProject {
+impl BuildSpecParser for PyProject {
     fn from_file(path: &Path) -> Result<Self, Box<dyn Error>>
     where
         Self: Sized,
@@ -183,10 +288,7 @@ mod test {
         let r = Requirements::from_file(&path).unwrap();
         assert_eq!(
             r.requires,
-            vec![
-                ("flask".to_string(), None),
-                ("pydantic".to_string(), Some("2.x".to_string()))
-            ]
+            vec!["flask".to_string(), "pydantic==2.x".to_string()]
         );
     }
 
@@ -195,16 +297,14 @@ mod test {
         let curr_dir = env::current_dir().unwrap();
         let path_str = format!("{}/tests/static/setup.py", curr_dir.to_str().unwrap());
         let path = Path::new(&path_str);
+        let s = ast::Suite::parse(read_file(&path).unwrap().as_str(), path_str.as_str());
         let s = SetupPy::from_file(&path).unwrap();
         assert_eq!(s.package_name, "babelone-test");
         assert_eq!(s.version, Some("2.0".to_string()));
         assert_eq!(s.dev_requires, None);
         assert_eq!(
             s.install_requires,
-            Some(vec![
-                ("pydantic".to_string(), Some("2.6.2".to_string())),
-                ("fastapi".to_string(), None)
-            ])
+            Some(vec!["pydantic==2.6.2".to_string(), "fastapi".to_string(),])
         );
     }
 
@@ -217,16 +317,20 @@ mod test {
         let build_system = p.build_system.unwrap();
         let project = p.project.unwrap();
         assert_eq!(&build_system.requires, &Some(vec!["hatchling".to_string()]));
-        assert_eq!(&build_system.build_backend, &Some("hatchling.build".to_string()));
+        assert_eq!(
+            &build_system.build_backend,
+            &Some("hatchling.build".to_string())
+        );
         assert_eq!(&project.name, "spam-eggs");
         assert_eq!(&project.version, "2020.0.0");
-        assert_eq!(&project.dependencies, &Some(
-            vec![
-              "httpx".to_string(),
-              "gidgethub[httpx]>4.0.0".to_string(),
-              "django>2.1; os_name != 'nt'".to_string(),
-              "django>2.0; os_name == 'nt'".to_string(),
-            ]
-        ));
+        assert_eq!(
+            &project.dependencies,
+            &Some(vec![
+                "httpx".to_string(),
+                "gidgethub[httpx]>4.0.0".to_string(),
+                "django>2.1; os_name != 'nt'".to_string(),
+                "django>2.0; os_name == 'nt'".to_string(),
+            ])
+        );
     }
 }
--- tests/static/setup.py
+++ tests/static/setup.py
@@ -1,10 +1,12 @@
 import setuptools
 
+VERSION = "2.0"
+
 
 if __name__ == "__main__":
     setuptools.setup(
         name="babelone-test",
-        version="2.0",
+        version=VERSION,
         author="Rahul D. Ghosal",
         install_requires=[
             "pydantic==2.6.2",
