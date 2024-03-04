08b31eb9d42feb609f7093330c3d7e38cf1ec2d5
Rahul Ghosal <rdghosal@gmail.com>
2024-03-04 08:50:46 -0500

    feat: add cli and script thereto

---
+++ python/babelone_cli/main.py
@@ -0,0 +1,23 @@
+from babelone_cli import _babelone_core
+import rich_click as click
+
+
+@click.command(name="babelone")
+@click.rich_config(
+    help_config=click.RichHelpConfiguration(
+        use_markdown=True, width=60, show_arguments=True
+    )
+)
+@click.argument("source", required=True)
+@click.argument("destination", required=True)
+def translate(source: str, destination: str):
+    """Translates a Python package build specification file from one
+    format to another, e.g., setup.py -> pyproject.toml.
+
+    """
+    _babelone_core.translate(source, destination)
+
+
+translate()
+# if __name__ == "__main__":
+#     translate()
