e6ee70971ee101a23d3cfe0543b95bf86e15e8ff
Rahul Ghosal <rdghosal@gmail.com>
2024-03-05 08:05:37 -0500

    feat: add mapping logic to complete translations between all spec types

--- src/specs.rs
+++ src/specs.rs
@@ -1,6 +1,6 @@
 //! Models encapsulating Python package build specifications.
-use serde::{Deserialize, Serialize};
 use pyo3::prelude::*;
+use serde::{Deserialize, Serialize};
 
 #[pyclass]
 pub enum PyBuildSpec {
@@ -52,7 +52,85 @@ pub struct Project {
     pub dependencies: Option<Vec<String>>,
 }
 
+impl Requirements {
+    pub fn from_setup(setup: Setup) -> Self {
+        // TODO: handle extra_requires.
+        let mut requires = Vec::<String>::new();
+        if let Some(mut install_requires) = setup.install_requires {
+            requires.append(&mut install_requires);
+        }
+        if let Some(mut setup_requires) = setup.setup_requires {
+            requires.append(&mut setup_requires);
+        }
+        Self { requires }
+    }
+
+    pub fn from_pyproject(pyproject: PyProject) -> Self {
+        let mut requires = Vec::<String>::new();
+        if let Some(project) = pyproject.project {
+            if let Some(mut dependencies) = project.dependencies {
+                requires.append(&mut dependencies);
+            }
+        }
+        if let Some(build_system) = pyproject.build_system {
+            if let Some(mut reqs) = build_system.requires {
+                requires.append(&mut reqs);
+            }
+        }
+        Self { requires }
+    }
+}
+
+impl Setup {
+    pub fn from_requirements(requirements: Requirements) -> Self {
+        let install_requires = Some(requirements.requires);
+        Self {
+            install_requires,
+            setup_requires: None,
+            dev_requires: None,
+            package_name: None,
+            version: None,
+        }
+    }
+
+    pub fn from_pyproject(pyproject: PyProject) -> Self {
+        // TODO: handle extra_requires
+        let (package_name, version, install_requires) = if pyproject.project.is_some() {
+            let project = pyproject.project.unwrap();
+            (project.name, project.version, project.dependencies)
+        } else {
+            (None, None, None)
+        };
+        let setup_requires = if pyproject.build_system.is_some() {
+            let build_system = pyproject.build_system.unwrap();
+            build_system.requires
+        } else {
+            None
+        };
+        Self {
+            package_name,
+            version,
+            install_requires,
+            setup_requires,
+            dev_requires: None,
+        }
+    }
+}
 impl PyProject {
+    pub fn from_requirements(requirements: Requirements) -> Self {
+        let dependencies = Some(requirements.requires);
+        let build_system = None;
+        let project = Some(Project {
+            name: None,
+            version: None,
+            dependencies,
+        });
+        Self {
+            project,
+            build_system,
+        }
+    }
+
     pub fn from_setup(setup: Setup) -> Self {
         let name = setup.package_name;
         let version = setup.version;
