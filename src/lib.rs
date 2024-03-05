72e754e112281463b9aefb6da22f0747ae47b1b7
Rahul Ghosal <rdghosal@gmail.com>
2024-03-05 08:13:49 -0500

    feat: add mappings between all valid file types

--- src/lib.rs
+++ src/lib.rs
@@ -39,11 +39,41 @@ fn translate(source: String, destination: String) -> PyResult<()> {
     let source_type = get_spec_type(&source)?;
     let dest_type = get_spec_type(&destination)?;
     match (source_type, dest_type) {
+        // TODO: Replace unwraps.
+        (specs::PyBuildSpec::Requirements, specs::PyBuildSpec::PyProject) => {
+            let requirements = parsers::RequirementsParser::from_file(&source);
+            let pyproject = specs::PyProject::from_requirements(requirements.unwrap());
+            let _ = generators::PyProjectGenerator::make_file(&destination, &pyproject);
+            Ok(())
+        }
         (specs::PyBuildSpec::Setup, specs::PyBuildSpec::PyProject) => {
             let setup = parsers::SetupParser::from_file(&source);
-            // TODO: Replace unwrap.
             let pyproject = specs::PyProject::from_setup(setup.unwrap());
             let _ = generators::PyProjectGenerator::make_file(&destination, &pyproject);
+            Ok(())
+        }
+        (specs::PyBuildSpec::Requirements, specs::PyBuildSpec::Setup) => {
+            let requirements = parsers::RequirementsParser::from_file(&source);
+            let setup = specs::Setup::from_requirements(requirements.unwrap());
+            let _ = generators::SetupGenerator::make_file(&destination, &setup);
+            Ok(())
+        }
+        (specs::PyBuildSpec::PyProject, specs::PyBuildSpec::Setup) => {
+            let pyproject = parsers::PyProjectParser::from_file(&source);
+            let setup = specs::Setup::from_pyproject(pyproject.unwrap());
+            let _ = generators::SetupGenerator::make_file(&destination, &setup);
+            Ok(())
+        }
+        (specs::PyBuildSpec::Setup, specs::PyBuildSpec::Requirements) => {
+            let setup = parsers::SetupParser::from_file(&source);
+            let requirements = specs::Requirements::from_setup(setup.unwrap());
+            let _ = generators::RequirementsGenerator::make_file(&destination, &requirements);
+            Ok(())
+        }
+        (specs::PyBuildSpec::PyProject, specs::PyBuildSpec::Requirements) => {
+            let pyproject = parsers::PyProjectParser::from_file(&source);
+            let requirements = specs::Requirements::from_pyproject(pyproject.unwrap());
+            let _ = generators::RequirementsGenerator::make_file(&destination, &requirements);
             Ok(())
         }
         _ => Err(PyNotImplementedError::new_err("Failed to perform operation. Only conversions between setup.py and pyproject.toml are allowed.")),
